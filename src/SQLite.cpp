80d0c7212096a8b63381a86f5060d138d5e45180
Jaakko Koivuniemi <oh7bf@sral.fi>
2020-07-14 19:50:38 -0500

    SQLite.cpp functions to write data to SQLite database
    These are documented SQLite.hpp. They require database tables defined in
    tables.txt file.

---
+++ src/SQLite.cpp
@@ -0,0 +1,275 @@
+/**************************************************************************
+ * 
+ * SQLite class member functions. 
+ *       
+ * Copyright (C) 2020 Jaakko Koivuniemi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************
+ *
+ * Tue Jul 14 13:30:25 CDT 2020
+ * Edit: 
+ *
+ * Jaakko Koivuniemi
+ **/
+
+#include "SQLite.hpp"
+
+using namespace std;
+
+/// SQLite constructor to initialize all parameters.
+SQLite::SQLite(std::string file, std::string table, std::string insert_stmt)
+{
+  this->file = file;
+  this->table = table;
+  this->insert_stmt = insert_stmt;
+}
+
+SQLite::~SQLite() { };
+
+/// SQLite member function to open database connection and execute _select datetime()_.
+std::string SQLite::GetDateTime(int & error)
+{
+  sqlite3 *db;
+  sqlite3_stmt *stmt;
+  const char query[ 200 ] = "select datetime()";
+
+  char message[ 500 ] = "";
+  char dtime[ 500 ] = "";
+
+  int rc = sqlite3_open_v2(file.c_str(), &db, SQLITE_OPEN_READONLY, NULL);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Can not open database: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return "";
+  }
+
+  rc = sqlite3_prepare_v2(db, query, 200, &stmt, 0);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Statement prepare failed: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return "";
+  }
+  else
+  {
+    if( stmt != NULL )
+    {
+      rc = sqlite3_step( stmt );
+      if( rc != SQLITE_ROW )
+      {
+        sprintf(message, "Statement failed: %s", sqlite3_errmsg( db ) );
+        fprintf(stderr, SD_ERR "%s", message);
+        error = rc;
+        sqlite3_close( db );
+
+	return "";
+      }
+      else
+      {
+        sprintf(dtime, "%s", sqlite3_column_text(stmt, 0) );
+        fprintf(stderr, SD_DEBUG "SQLite: %s", dtime);
+        error = SQLITE_OK;
+      }
+    }
+  }
+
+  return dtime;
+}
+
+/// SQLite member function to open connection and execute insert statement on database table.
+bool SQLite::Insert(std::string name, int N, double *data, int & error)
+{
+  sqlite3 *db;
+  sqlite3_stmt *stmt;
+
+  char message[ 500 ] = "";
+
+  int rc = sqlite3_open_v2(file.c_str(), &db, SQLITE_OPEN_READWRITE, NULL);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Can not open database: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  }
+
+  int i;
+
+  rc = sqlite3_prepare_v2(db, insert_stmt.c_str(), 200, &stmt, 0);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Statement prepare failed: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  }
+  else
+  {
+    rc = sqlite3_bind_text(stmt, 1, name.c_str(), name.length(), SQLITE_STATIC);
+
+    if( rc != SQLITE_OK )
+    {
+      sprintf(message, "Binding failed: %s", sqlite3_errmsg( db ) );
+      fprintf(stderr, SD_ERR "%s", message);
+      error = rc;
+      sqlite3_close( db );
+      
+      return false;
+    }
+    else
+    {
+      for( i = 1; i <= N; i++)
+      {
+        rc = sqlite3_bind_double(stmt, i + 1, data[ i - 1 ]);
+
+    	if( rc != SQLITE_OK )
+        {
+          sprintf(message, "Binding failed: %s", sqlite3_errmsg( db ) );
+          fprintf(stderr, SD_ERR "%s", message);
+          error = rc;
+          sqlite3_close( db );
+        } 
+      }
+    }
+  }     
+
+  rc = sqlite3_step( stmt );
+
+  if( rc != SQLITE_DONE )
+  {
+    sprintf(message, "Statement failed: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  } 
+
+  rc = sqlite3_finalize( stmt );
+  sqlite3_close( db );
+
+  return true;
+}
+
+/// SQLite member function to open connection and execute insert statement on database table.
+bool SQLite::Insert(std::string name, int Nd, double *dbl_array, int Ni, int *int_array, int & error)
+{
+  sqlite3 *db;
+  sqlite3_stmt *stmt;
+
+  char message[ 500 ] = "";
+
+  int rc = sqlite3_open_v2(file.c_str(), &db, SQLITE_OPEN_READWRITE, NULL);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Can not open database: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  }
+
+  int i;
+
+  rc = sqlite3_prepare_v2(db, insert_stmt.c_str(), 200, &stmt, 0);
+
+  if( rc != SQLITE_OK )
+  {
+    sprintf(message, "Statement prepare failed: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  }
+  else
+  {
+    rc = sqlite3_bind_text(stmt, 1, name.c_str(), name.length(), SQLITE_STATIC);
+
+    if( rc != SQLITE_OK )
+    {
+      sprintf(message, "Binding failed: %s", sqlite3_errmsg( db ) );
+      fprintf(stderr, SD_ERR "%s", message);
+      error = rc;
+      sqlite3_close( db );
+      
+      return false;
+    }
+    else
+    {
+      for( i = 1; i <= Nd; i++)
+      {
+        rc = sqlite3_bind_double(stmt, i + 1, dbl_array[ i - 1 ]);
+
+    	if( rc != SQLITE_OK )
+        {
+          sprintf(message, "Binding failed: %s", sqlite3_errmsg( db ) );
+          fprintf(stderr, SD_ERR "%s", message);
+          error = rc;
+          sqlite3_close( db );
+        } 
+      }
+
+      for( i = 0; i < Ni; i++)
+      {
+        rc = sqlite3_bind_int(stmt, i + Nd + 2, int_array[ i ]);
+
+    	if( rc != SQLITE_OK )
+        {
+          sprintf(message, "Binding failed: %s", sqlite3_errmsg( db ) );
+          fprintf(stderr, SD_ERR "%s", message);
+          error = rc;
+          sqlite3_close( db );
+        } 
+      }
+    }
+  }     
+  
+  rc = sqlite3_step( stmt );
+
+  if( rc != SQLITE_DONE )
+  {
+    sprintf(message, "Statement failed: %s", sqlite3_errmsg( db ) );
+    fprintf(stderr, SD_ERR "%s", message);
+    error = rc;
+    sqlite3_close( db );
+
+    return false;
+  } 
+
+  rc = sqlite3_finalize( stmt );
+  sqlite3_close( db );
+
+  return true;
+}
+
---
+++ src/SQLite.hpp
@@ -0,0 +1,83 @@
+/**************************************************************************
+ * 
+ * SQLite database class definitions and constructor. 
+ *       
+ * Copyright (C) 2020 Jaakko Koivuniemi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************
+ *
+ * Tue Jul 14 10:58:25 CDT 2020
+ * Edit: 
+ *
+ * Jaakko Koivuniemi
+ **/
+
+#ifndef _SQLITE_HPP
+#define _SQLITE_HPP
+
+#include <systemd/sd-daemon.h>
+#include <string>
+#include <sqlite3.h>
+
+/// Class for SQLite database functions. 
+
+/// The constructor _SQLite_ sets SQLite database file name, table and insert 
+/// query for data storage.
+class SQLite 
+{
+    std::string file;         ///< SQLite database file name
+    std::string table;        ///< SQLite database table
+    std::string insert_stmt;  ///< SQLite insert statement 
+
+   public:
+    /// Construct Database object. 
+    SQLite();
+
+    /// Construct Database object with parameters.
+    SQLite(std::string file, std::string table, std::string ins_stmt);
+
+    virtual ~SQLite();
+
+    /// Get database file name.
+    std::string GetFile() { return file; }
+
+    /// Get database table name.
+    std::string GetTable() { return table; }
+
+    /// Get insert query.
+    std::string GetInsert() { return insert_stmt; }
+
+    /// Get return string from datetime() query.
+    std::string GetDateTime(int & error);
+
+    /// Set database file name.
+    void SetFile(std::string file) { this->file = file; }
+
+    /// Set database table name.
+    void SetTable(std::string table) { this->table = table; }
+
+    /// Set database insert query.
+    void SetInsert(std::string insert_stmt) { this->insert_stmt = insert_stmt; }
+
+    /// Insert name and N doubles to database table and return true in success.
+    bool Insert(std::string name, int N, double *data, int & error);
+
+    /// Insert name, N doubles and M integers to database table and return true in success.
+    bool Insert(std::string name, int Nd, double *dbl_array, int Ni, int *int_array, int & error);
+
+};
+
+#endif
