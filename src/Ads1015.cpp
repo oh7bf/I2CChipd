1d500d635b2d8c4c9687d9efb32713f0bf2067a5
Jaakko Koivuniemi <oh7bf@sral.fi>
2020-08-13 17:27:30 -0500

    Ads1015.hpp class for ADS1015 chip
    First untested version of the class similar to Tmp102.hpp.

---
+++ src/Ads1015.cpp
@@ -0,0 +1,318 @@
+/**************************************************************************
+ * 
+ * Ads1015 class member functions for configuration and reading with I2C. 
+ *       
+ * Copyright (C) 2020 Jaakko Koivuniemi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************
+ *
+ * Sat Aug  8 20:19:22 CDT 2020
+ * Edit: Thu Aug 13 16:40:15 CDT 2020
+ *
+ * Jaakko Koivuniemi
+ **/
+
+#include "Ads1015.hpp"
+
+using namespace std;
+
+Ads1015::~Ads1015() { };
+
+/// First pointer register is set to HIGH_THRESH register followed by two byte 
+/// transfer from the chip. 
+int16_t Ads1015::GetHighThreshold()
+{
+  int16_t HighThreshold = 0;
+
+  I2Chip::I2cWriteUInt8(ADS1015_HIGH_THRESH_REG, address, buffer, error);
+  HighThreshold = (int16_t)I2Chip::I2cReadUInt16(address, buffer, error);
+
+  return HighThreshold;
+}
+
+/// First pointer register is set to LOW_THRESH register followed by two byte 
+/// transfer from the chip. 
+int16_t Ads1015::GetLowThreshold()
+{
+  int16_t LowThreshold = 0;
+
+  I2Chip::I2cWriteUInt8(ADS1015_LOW_THRESH_REG, address, buffer, error);
+  LowThreshold = (int16_t)I2Chip::I2cReadUInt16(address, buffer, error);
+
+  return LowThreshold;
+} 
+
+/// First pointer register is set to CONFIG register followed by two byte 
+/// transfer from the chip. 
+uint16_t Ads1015::GetConfig()
+{
+  uint16_t Config = 0;
+
+  I2Chip::I2cWriteUInt8(ADS1015_CONFIG_REG, address, buffer, error);
+  Config = I2Chip::I2cReadUInt16(address, buffer, error);
+
+  return Config;
+} 
+
+/// The CONFIG register is read to get programmable gain amplifier setting.
+uint16_t Ads1015::GetPGA()
+{
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  pga = ( (Config & 0x0E00 ) >> 9);
+
+  return pga;
+}
+
+/// The CONFIG register is read to get polarity bit.
+uint16_t Ads1015::GetCompPolarity()
+{
+  uint16_t CompPol = 2;
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  CompPol = ( Config & 0x0008 ) >> 3;
+
+  return CompPol;
+}
+
+/// The CONFIG register is read to get latching bit.
+uint16_t Ads1015::GetCompLatching()
+{
+  uint16_t CompLatch = 2;
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  CompLatch = ( Config & 0x0004 ) >> 2;
+
+  return CompLatch;
+}
+
+/// The CONFIG register is read to get two queue bits.
+uint16_t Ads1015::GetCompQueue()
+{
+  uint16_t CompQueue = 5;
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  CompQueue = Config & 0x0003;
+
+  return CompQueue;
+}
+
+/// The CONFIG register is read to get three data rate bits.
+uint16_t Ads1015::GetDataRate()
+{
+  uint16_t Rate = 8;
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  Rate = ( Config & 0x00E0 ) >> 5;
+
+  return Rate;
+}
+
+/// Write one byte to chip to change pointer value for reading registers.
+void Ads1015::SetPointer(uint8_t PointerReg)
+{
+  I2Chip::I2cWriteUInt8(PointerReg, address, buffer, error);
+}
+
+/// Write two bytes to HIGH_THRESH register.
+void Ads1015::SetHighThreshold(int16_t HighThreshold)
+{
+  I2Chip::I2cWriteRegisterUInt16(ADS1015_HIGH_THRESH_REG, HighThreshold, address, buffer, error);
+}
+
+/// Write two bytes to LOW_THRESH register.
+void Ads1015::SetLowThreshold(int16_t LowThreshold)
+{
+  I2Chip::I2cWriteRegisterUInt16(ADS1015_LOW_THRESH_REG, LowThreshold, address, buffer, error);
+}
+
+/// Write two bytes to CONFIG register.
+void Ads1015::SetConfig(uint16_t Config)
+{
+  I2Chip::I2cWriteRegisterUInt16(ADS1015_CONFIG_REG, Config, address, buffer, error);
+}
+
+/// Set three PGA bits in CONFIG register.
+void Ads1015::SetPGA(uint16_t PGA)
+{
+  uint16_t Config = 0;
+
+  PGA &= 0x0007;
+  pga = PGA;
+  PGA = PGA << 9;
+  Config = Ads1015::GetConfig();
+  Config &= 0xF1FF;
+  Config |= PGA; 
+  
+  Ads1015::SetConfig( Config );
+
+}
+
+/// Set two queue bits in CONFIG register.
+void Ads1015::SetCompQueue(uint16_t CompQueue)
+{
+  uint16_t Config = 0;
+
+  CompQueue &= 0x0003;
+  Config = Ads1015::GetConfig();
+  Config &= 0xFFFC;
+  Config |= CompQueue; 
+  
+  Ads1015::SetConfig( Config );
+
+}
+
+/// Set three data rate bits in CONFIG register.
+void Ads1015::SetDataRate(uint16_t DataRate)
+{
+  uint16_t Config = 0;
+
+  DataRate &= 0x0007;
+  DataRate = DataRate << 5;
+  Config = Ads1015::GetConfig();
+  Config &= 0xFF1F;
+  Config |= DataRate; 
+
+  Ads1015::SetConfig( Config );
+
+}
+
+/// Set polarity bit in CONFIG register.
+void Ads1015::SetCompPolarity(uint16_t CompPolarity)
+{
+  uint16_t Config = 0;
+
+  CompPolarity &= 0x0001;
+  CompPolarity = CompPolarity << 3;
+  Config = Ads1015::GetConfig();
+  Config &= 0xFFF7;
+  Config |= CompPolarity; 
+
+  Ads1015::SetConfig( Config );
+
+}
+
+/// Set latching bit in CONFIG register.
+void Ads1015::SetCompLatching(uint16_t CompLatching)
+{
+  uint16_t Config = 0;
+
+  CompLatching &= 0x0001;
+  CompLatching = CompLatching << 2;
+  Config = Ads1015::GetConfig();
+  Config &= 0xFFFB;
+  Config |= CompLatching; 
+
+  Ads1015::SetConfig( Config );
+}
+
+/// Set comparator mode bit in CONFIG register.
+void Ads1015::SetCompMode(uint16_t CompMode)
+{
+  uint16_t Config = 0;
+
+  CompMode &= 0x0001;
+  CompMode = CompMode << 8;
+  Config = Ads1015::GetConfig();
+  Config &= 0xFEFF;
+  Config |= CompMode; 
+
+  Ads1015::SetConfig( Config );
+}
+
+/// Set MODE = 1 in CONFIG register.
+void Ads1015::Powerdown()
+{
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  Config |= 0x0100;
+  Ads1015::SetConfig( Config );
+}
+
+/// Set MODE = 0 in CONFIG register.
+void Ads1015::Continuous()
+{
+  uint16_t Config = 0;
+
+  Config = Ads1015::GetConfig();
+  Config &= 0xFEFF;
+  Ads1015::SetConfig( Config );
+}
+
+/// Set OS = 1 in CONFIG register.
+void Ads1015::SingleConversion()
+{
+  uint16_t Config = 0;
+  Config = Ads1015::GetConfig();
+  Config |= 0x8000; 
+  Ads1015::SetConfig( Config );
+}
+
+/// Read two bytes from chip. SetPointer( ADS1015_CONV_REG ) needs to be 
+/// called before using this function. Private variable _pga_ is used
+/// in conversion to voltage. 
+bool Ads1015::ReadConversion()
+{
+  bool success = true;
+
+  adc = (int16_t)I2Chip::I2cReadUInt16(address, buffer, error);
+
+  if( error != 0 )
+  {
+    success = false;
+  }
+  else
+  {
+    switch( pga )
+    {
+      case 0:
+        Voltage = adc * 6.144 / 32767;
+        break;
+      case 1:
+        Voltage = adc * 4.096 / 32767;
+	break;
+      case 2:
+        Voltage = adc * 2.048 / 32767;
+        break;
+      case 3:
+        Voltage = adc * 1.024 / 32767;
+        break;
+      case 4:
+        Voltage = adc * 0.512 / 32767;
+        break;
+      case 5:
+        Voltage = adc * 0.256 / 32767;
+        break;
+      case 6:
+        Voltage = adc * 0.256 / 32767;
+        break;
+      case 7:
+        Voltage = adc * 0.256 / 32767;
+	break;
+      default:
+	success = false;
+    }
+  }
+
+  return success;
+}
+
---
+++ src/Ads1015.hpp
@@ -0,0 +1,176 @@
+/**************************************************************************
+ * 
+ * Ads1015 class definitions and constructor. Base class is I2Chip. 
+ *       
+ * Copyright (C) 2020 Jaakko Koivuniemi.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************
+ *
+ * Sat Aug  8 19:34:10 CDT 2020
+ * Edit: 
+ *
+ * Jaakko Koivuniemi
+ **/
+
+#ifndef _ADS1015_HPP
+#define _ADS1015_HPP
+
+#include "I2Chip.hpp"
+
+#define ADS1015_CONV_REG 0x00
+#define ADS1015_CONFIG_REG 0x01
+#define ADS1015_LOW_THRESH_REG 0x02
+#define ADS1015_HIGH_THRESH_REG 0x03
+
+/// Class for Ads1015 inherited from I2Chip base class. 
+
+/// The constructor _Ads1015_ sets name tag, device file name and chip address
+/// used in data transfer. 
+class Ads1015 : public I2Chip 
+{
+    std::string name;       ///< name tag for chip
+
+    std::string i2cdev;     ///< device file for writing and reading serial data
+    uint16_t address;       ///< I2C chip address
+    int error;              ///< error flag for I2C communication
+
+    /// buffer to transfer serial data to and from chip
+    uint8_t buffer[ BUFFER_MAX ] = { };
+
+    double Voltage;    ///< voltage from last reading
+    uint16_t pga;      ///< programmable gain from last setting
+    int16_t adc;       ///< conversion register value from last reading
+
+   public:
+    /// Construct Ads1015 object with parameters.
+    Ads1015(std::string name, std::string i2cdev, uint16_t address) : I2Chip(name, i2cdev, address)
+   {
+      this->name = name;
+      this->i2cdev = i2cdev;
+      this->address = address;
+   };
+
+    /// Construct Ads1015 object with parameters.
+    Ads1015(std::string name, std::string i2cdev) : I2Chip(name, i2cdev, 0x49)
+   {
+      this->name = name;
+      this->i2cdev = i2cdev;
+   };
+
+    virtual ~Ads1015();
+
+    /// Get chip name tag.
+    std::string GetName() { return name; }
+
+    /// Get chip device file name.
+    std::string GetDevice() { return i2cdev; }
+
+    /// Get I2C chip address in use now.
+    uint16_t GetAddress() { return address; }
+
+    /// Get last error number.
+    int GetError() { return error; }
+
+    /// Get voltage from last reading. 
+    double GetVoltage() { return Voltage; }
+
+    /// Get programmable gain amplifier setting 0 - 7.
+    uint16_t GetPGA(); 
+
+    /// Get high threshold register value.
+    int16_t GetHighThreshold();
+
+    /// Get low threshold register value.
+    int16_t GetLowThreshold(); 
+
+    /// Get configuration register value.
+    uint16_t GetConfig(); 
+
+    /// Get comparator pin polarity to 0 or 1.
+    uint16_t GetCompPolarity();
+
+    /// Get comparator latching setting 0 or 1.
+    uint16_t GetCompLatching();
+
+    /// Get comparator queue value 0 - 3.
+    uint16_t GetCompQueue();
+
+    /// Get data rate setting 0 - 7.
+    uint16_t GetDataRate();
+
+    /// Set chip name tag.
+    void SetName(std::string name) { this->name = name; }
+
+    /// Set chip device file name.
+    void SetDevice(std::string i2cdev) { this->i2cdev = i2cdev; }
+
+    /// Set I2C bus address 0x48.
+    void SetAddress0x48() {this->address = 0x48; }
+
+    /// Set I2C bus address 0x49.
+    void SetAddress0x49() {this->address = 0x49; }
+
+    /// Set I2C bus address 0x4A.
+    void SetAddress0x4A() {this->address = 0x4A; }
+
+    /// Set I2C bus address 0x4B.
+    void SetAddress0x4B() {this->address = 0x4B; }
+
+    /// Set pointer register.
+    void SetPointer(uint8_t PointerReg);
+
+    /// Set high threshold register value.
+    void SetHighThreshold(int16_t HighThreshold);
+
+    /// Set low limit register value.
+    void SetLowThreshold(int16_t LowThreshold); 
+
+    /// Set configuration register value.
+    void SetConfig(uint16_t Config); 
+
+    /// Set programmable gain amplifier 0 - 7.
+    void SetPGA(uint16_t PGA);
+
+    /// Set comparator pin polarity to 0 or 1.
+    void SetCompPolarity(uint16_t CompPolarity);
+
+    /// Set comparator latching setting 0 or 1.
+    void SetCompLatching(uint16_t CompLatching);
+
+    /// Set comparator mode setting 0 or 1.
+    void SetCompMode(uint16_t CompMode);
+
+    /// Set comparator queue value 0 - 3.
+    void SetCompQueue(uint16_t CompQueue);
+
+    /// Set data rate setting 0 - 7.
+    void SetDataRate(uint16_t DataRate);
+
+    /// Powerdown mode to save power.
+    void Powerdown();
+
+    /// Continuous conversion mode.
+    void Continuous();
+
+    /// In Powerdown Mode start single conversion.
+    void SingleConversion(); 
+
+    /// Read chip conversion register and return true if success.
+    bool ReadConversion();
+
+};
+
+#endif
